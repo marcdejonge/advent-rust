92e4e97c8aea4668b3e4ae85a250ee5898fc6d1b
Marc de Jonge <marc.dejonge@jamf.com>
2023-04-11 16:08:46 +0200

    Replaced the Regex in Day 4 with a simpler String parser

--- 2022/src/bin/day4.rs
+++ 2022/src/bin/day4.rs
@@ -1,25 +1,27 @@
 use advent_lib::day::*;
-use regex::Regex;
-use std::ops::RangeInclusive;
+use prse::Parse;
+use prse_derive::parse;
 
 struct Day {
-    range_pairs: Vec<(RangeInclusive<u32>, RangeInclusive<u32>)>,
+    range_pairs: Vec<(Range, Range)>,
+}
+
+#[derive(Parse)]
+#[prse = "{from}-{to}"]
+struct Range {
+    from: u32,
+    to: u32,
+}
+
+impl Range {
+    fn contains(&self, value: u32) -> bool { value >= self.from && value <= self.to }
+    fn wraps(&self, other: &Range) -> bool { self.from <= other.from && self.to >= other.to }
 }
 
 impl FromIterator<String> for Day {
     fn from_iter<T: IntoIterator<Item = String>>(iter: T) -> Self {
-        let re = Regex::new("(\\d+)-(\\d+),(\\d+)-(\\d+)").unwrap();
         Day {
-            range_pairs: iter
-                .into_iter()
-                .map(|line| {
-                    let cap = re.captures(&line).unwrap();
-                    (
-                        RangeInclusive::new(cap[1].parse().unwrap(), cap[2].parse().unwrap()),
-                        RangeInclusive::new(cap[3].parse().unwrap(), cap[4].parse().unwrap()),
-                    )
-                })
-                .collect(),
+            range_pairs: iter.into_iter().map(|line: String| parse!(line, "{},{}")).collect(),
         }
     }
 }
@@ -30,7 +32,7 @@ impl ExecutableDay for Day {
     fn calculate_part1(&self) -> Self::Output {
         self.range_pairs
             .iter()
-            .filter(|(first, second)| is_contained(first, second) || is_contained(second, first))
+            .filter(|(first, second)| first.wraps(second) || second.wraps(first))
             .count()
     }
 
@@ -38,17 +40,13 @@ impl ExecutableDay for Day {
         self.range_pairs
             .iter()
             .filter(|(first, second)| {
-                first.contains(&second.start())
-                    || first.contains(&(second.end()))
-                    || second.contains(&first.start())
-                    || second.contains(&(first.end()))
+                first.contains(second.from)
+                    || first.contains(second.to)
+                    || second.contains(first.from)
+                    || second.contains(first.to)
             })
             .count()
     }
-}
-
-fn is_contained<T: PartialOrd<T>>(outside: &RangeInclusive<T>, inside: &RangeInclusive<T>) -> bool {
-    outside.start() <= inside.start() && outside.end() >= inside.end()
 }
 
 fn main() { execute_day::<Day>() }
