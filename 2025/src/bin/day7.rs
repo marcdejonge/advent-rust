e3d45e4033e18c6dbf6cb165db6a507489bcac3f
Marc de Jonge <marc.dejonge@jamf.com>
2025-12-07 16:11:16 +0100

    Solved day 7 of 2025

---
+++ 2025/src/bin/day7.rs
@@ -0,0 +1,54 @@
+#![feature(test)]
+
+use advent_lib::{direction::Direction::*, grid::*, iter_utils::CountIf, *};
+use advent_macros::FromRepr;
+
+#[repr(u8)]
+#[derive(FromRepr, PartialEq, Eq)]
+enum Field {
+    Start = b'S',
+    Split = b'^',
+    Empty = b'.',
+    Beam = b'|',
+}
+
+struct Memoize(Grid<usize>);
+
+impl Memoize {
+    fn new(grid: &Grid<Field>) -> Self { Self(grid.map(|_| usize::MAX)) }
+
+    fn split_count_from_start(&mut self, grid: &Grid<Field>) -> usize {
+        self.split_count(grid, grid.find(|field| *field == Field::Start).unwrap())
+    }
+
+    fn split_count(&mut self, grid: &Grid<Field>, from: Location) -> usize {
+        let mut cached = self.0.get(from).cloned().unwrap_or(0);
+        if cached == usize::MAX {
+            cached = match grid.get(from) {
+                None => 0,
+                Some(Field::Split) => {
+                    self.split_count(grid, from + West) + self.split_count(grid, from + East) + 1
+                }
+                _ => self.split_count(grid, from + South),
+            };
+            *self.0.get_mut(from).unwrap() = cached;
+        }
+        cached
+    }
+}
+
+fn calculate_part1(grid: &Grid<Field>) -> usize {
+    let mut mem = Memoize::new(grid);
+    mem.split_count_from_start(grid);
+    grid.count_if(|&(loc, f)| f == &Field::Split && mem.0[loc] != usize::MAX)
+}
+
+fn calculate_part2(grid: &Grid<Field>) -> usize {
+    let mut mem = Memoize::new(grid);
+    mem.split_count_from_start(grid) + 1
+}
+
+day_main!(Grid<Field>);
+
+day_test!( 7, example => 21, 40);
+day_test!( 7 => 1585, 16716444407407 );
