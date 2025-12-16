ee320bc5035a322eb3aab6bd54fd641cad82e10e
Marc de Jonge <marc.dejonge@jamf.com>
2025-12-16 14:02:27 +0100

    [Day8] Some optimalisations

--- 2025/src/bin/day8.rs
+++ 2025/src/bin/day8.rs
@@ -1,4 +1,5 @@
 #![feature(test)]
+#![feature(iter_collect_into)]
 
 use advent_lib::{disjoint_set::DisjointSet, *};
 use itertools::Itertools;
@@ -6,39 +7,42 @@ use nom_parse_macros::parse_from;
 use rayon::prelude::*;
 
 type Point = advent_lib::geometry::Point<3, i64>;
+type DistancedPair = (i64, usize, usize);
 
 #[parse_from]
 struct Input {
     points: Vec<Point>,
     #[derived(sorted_distances(&points))]
-    sorted_distances: Vec<(usize, usize, i64)>,
+    sorted_distances: Vec<DistancedPair>,
     #[derived(points.len())]
     len: usize,
 }
 
-fn sorted_distances(points: &[Point]) -> Vec<(usize, usize, i64)> {
-    let mut result: Vec<(usize, usize, i64)> = (0..points.len())
-        .par_bridge()
-        .flat_map_iter(|left_ix| {
-            ((left_ix + 1)..points.len()).map(move |right_ix| {
-                let dist_vec = points[right_ix] - points[left_ix];
-                let dist = (dist_vec.x() * dist_vec.x()
+fn sorted_distances(points: &[Point]) -> Vec<DistancedPair> {
+    let mut result = Vec::with_capacity(points.len() * points.len() / 2);
+
+    points
+        .iter()
+        .enumerate()
+        .flat_map(|(left_ix, left)| {
+            points.iter().enumerate().skip(left_ix + 1).map(move |(right_ix, right)| {
+                let dist_vec = *right - *left;
+                let distance = dist_vec.x() * dist_vec.x()
                     + dist_vec.y() * dist_vec.y()
-                    + dist_vec.z() * dist_vec.z())
-                .isqrt();
-                (left_ix, right_ix, dist)
+                    + dist_vec.z() * dist_vec.z();
+                (distance, left_ix, right_ix)
             })
         })
-        .collect();
+        .collect_into(&mut result);
 
-    result.par_sort_unstable_by_key(|&(_, _, weight)| weight);
+    result.par_sort_unstable();
 
     result
 }
 
 fn calculate_part1(input: &Input) -> usize {
     let mut links = DisjointSet::with_len(input.len);
-    for &(left_ix, right_ix, _) in
+    for &(_, left_ix, right_ix) in
         input.sorted_distances.iter().take(if input.len < 100 { 10 } else { 1000 })
     {
         links.join(left_ix, right_ix);
@@ -49,10 +53,10 @@ fn calculate_part1(input: &Input) -> usize {
 
 fn calculate_part2(input: &Input) -> i64 {
     let mut links = DisjointSet::with_len(input.len);
-    let &(left_ix, right_ix, _) = input
+    let &(_, left_ix, right_ix) = input
         .sorted_distances
         .iter()
-        .filter(|&&(left_ix, right_ix, _)| links.join(left_ix, right_ix))
+        .filter(|&&(_, left_ix, right_ix)| links.join(left_ix, right_ix))
         .nth(input.len - 2) // The 0th item already joined the first 2, so subtract 2
         .unwrap();
 
